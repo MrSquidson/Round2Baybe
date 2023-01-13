98e93b35e0ef99346773da3ac5226cb485b702cf
Magnus H <mfh1106@gmail.com>
2023-01-13 13:17:47 +0100

    Tilføjede Except clauses til tal faserne for at give en mere detaljeret exit kode

--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 navnet = input("Skriv dit navn")
 print('Hej', navnet)
 print('Jeg er et program der skal hjælpe dig med at lægge 2 tal sammen... Er du klar?')
-tal1 = int(input("Skriv dit første tal"))
+
+try:
+    tal1 = int(input("Skriv dit første tal"))
+except:
+    print('Det virker ikke')
+    exit('Ikke et Realt tal')
+
 print('Dit første tal er:', tal1)
 
-tal2 = int(input('Skriv dit andet tal'))
+try:
+    tal2 = int(input('Skriv dit andet tal'))
+except:
+    print('Det virker ikke')
+    exit('Ikke et Realt tal')
+
 print('Dit andet tal er', tal2)
 
 print('Din samlede værdi mellem de to tal er', tal1 + tal2)
