641c009d50c034f16b4c754b4c2fe3a20c9b8ca4
Magnus H <mfh1106@gmail.com>
2023-01-17 12:53:00 +0100

    Er Resultatet et primtal? Og generalle fixes - Tilføjede "Broad" exceptions for at 'except' ikke længere er tom. Fik hjælp fra den bedste hjemmeside (https://stackoverflow.com/questions/4990718/how-can-i-write-a-try-except-block-that-catches-all-exceptions)
    - Låner et primtal script fra nettet (https://www.edureka.co/blog/python-program-prime-number/)

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-#Opgave Læg to bruger bestemte værdier sammen
-
-#Beder brugeren om deres navn for en mere personlig **Experience**
+# Opgave Læg to bruger bestemte værdier sammen
+#
+# Beder brugeren om deres navn for en mere personlig **Experience**
 navnet = input("Skriv dit navn")
 print('Hej', navnet)
 print('Jeg er et program der skal hjælpe dig med at lægge 2 tal sammen... Er du klar?')
 
 while True:
 
-    #Indsamler tal 1
+    #   Indsamler tal 1
     try:
         tal1 = int(input("Skriv dit første tal"))
 #       Hvis tallet ikke kan godkendes af maskinen eller
 #       brugeren har indtastet et bogstav giver den en besked + fejlkode
-    except:
+    except Exception as e:
         print('Det virker ikke')
         exit('Ikke et Realt tal')
 
     print('Dit første tal er:', tal1)
 
-#Indsamler tal 2
+#   Indsamler tal 2
     try:
         tal2 = int(input('Skriv dit andet tal'))
-#Se linje 11
-    except:
+#       Hvis tallet ikke kan godkendes af maskinen eller
+#       brugeren har indtastet et bogstav giver den en besked + fejlkode
+    except Exception as e:
         print('Det virker ikke')
         exit('Ikke et Realt tal')
 
@@ -30,14 +31,24 @@ while True:
 
     resultat = tal1 + tal2
 
-#Skaber en string der vil fortælle os om tallet er lige el. ulige
+#   Skaber en string der vil fortælle os om tallet er lige el. ulige
     ligeEllerUlige = 'ikke bestemt'
-#Tjekker om tallet er lige eller ulige
+#   Tjekker om tallet er lige eller ulige
     if (resultat % 2) == 0:
         ligeEllerUlige = 'lige'
     else:
         ligeEllerUlige = 'ulige'
 
-#Lægger de 2 tal der er indsamlet i int strings sammen og viser om tallet er lige el. ulige på samme linje
+#   Lægger de 2 tal der er indsamlet i int strings sammen og viser om tallet er lige el. ulige på samme linje
     print('Din samlede værdi mellem de to tal er', resultat, 'resultatet er et', ligeEllerUlige, 'tal')
 
+#   Finder ud af om resultatet er et primtal
+    if resultat > 1:
+        for i in range(2, resultat//2):
+            if (resultat % i) == 0:
+                print('Dit resultat', resultat, "er ikke et primtal")
+                break
+            else:
+                print('Dit resultat', resultat, "er et primtal")
+    else:
+        print('Dit resultat', resultat, "er ikke et primtal")
