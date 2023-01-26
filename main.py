e7e8e1c45b41d9144bc8a1ce2e9f84230747dfb9
Magnus H <mfh1106@gmail.com>
2023-01-26 14:40:15 +0100

    Fixed a bug where certain prime numbers would cause too many "is prime number" to be fired Added more color Moved around different code (to make it a tini-tiny bit cleaner Added more comments (My beloved)

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 # Opgave Læg to bruger bestemte værdier sammen
 # Importere textwrap
 import textwrap
-
+# Importere colorama og tillader farvede outputs i console
+from colorama import Fore
 # Beder brugeren om deres navn for en mere personlig **Experience**
-navnet = input("Skriv dit navn: ")
-print('Hej', navnet)
-print('Jeg er et program der skal hjælpe dig med at lægge 2 tal sammen... Er du klar?')
+navnet = input(Fore.BLUE + "NumZ: Skriv dit navn: ")
+print(Fore.BLUE + 'NumZ: Hej', navnet)
+print(Fore.BLUE + 'NumZ: Jeg er et program der skal hjælpe dig med at lægge 2 tal sammen... Er du klar?')
 
 # Laver strings til wrapper så hvis man inputer et meget stort tal kommer det tilbage på linjen
 preferredWidth = 70
 
 # Looper det hele
 while True:
-    # Importere colorama og tillader farvede outputs i console
-    from colorama import Fore, Back, Style
-
     # Laver Prefixes med custom farve ved hjælp af colorama
     prefix = Fore.GREEN + navnet + ": "
     prog = Fore.BLUE + "NumZ: "
@@ -23,8 +21,8 @@ while True:
     wrapper = textwrap.TextWrapper(initial_indent=prog, width=preferredWidth, subsequent_indent=' ' * len(prog))
     #   Indsamler tal 1
     try:
-
         print(wrapper.fill(Fore.BLUE + "Skriv dit første tal"))
+        # int sørger for at string'en der er indtastet er et tal/integer
         tal1 = int(input(prefix))
 #       Hvis tallet ikke kan godkendes af maskinen eller
 #       brugeren har indtastet et bogstav giver den en besked + fejlkode
@@ -32,7 +30,7 @@ while True:
         print(wrapper.fill(Fore.RED + '\tDet virker ikke'))
         exit(wrapper.fill(Fore.RED + '\tIkke et Realt tal'))
 
-    print(Fore.LIGHTCYAN_EX + '\t\tDit første tal er:', tal1)
+    print(Fore.LIGHTCYAN_EX + '\t\t\tDit første tal er:', tal1)
 
 #   Indsamler tal 2
     try:
@@ -44,7 +42,7 @@ while True:
         print(wrapper.fill(Fore.RED + '\tDet virker ikke'))
         exit(wrapper.fill(Fore.RED + '\tIkke et Realt tal'))
 
-    print(Fore.LIGHTCYAN_EX + '\t\tDit andet tal er', tal2)
+    print(Fore.LIGHTCYAN_EX + '\t\t\tDit andet tal er', tal2)
 
     resultat = tal1 + tal2
 
@@ -57,15 +55,22 @@ while True:
         ligeEllerUlige = Fore.RED + 'ulige'
 
 #   Lægger de 2 tal der er indsamlet i int strings sammen og viser om tallet er lige el. ulige på samme linje
-    print(Fore.LIGHTBLUE_EX + '\t\tDin samlede værdi mellem de to tal er', resultat, 'resultatet er et', ligeEllerUlige, 'tal')
+    print(Fore.LIGHTBLUE_EX + '\t\t\tDin samlede værdi mellem de to tal er', resultat, 'resultatet er et', ligeEllerUlige, 'tal')
 
 #   Finder ud af om resultatet er et primtal
+#   Break efter et primtal resultat sørger for at tal som 4090 + 3 ikke bliver printet 4090 gange
+#   Tak til @Kneecaptheif for at snakke om breakpoints og debug værktøjet
     if resultat > 1:
         for i in range(2, resultat//2):
             if (resultat % i) == 0:
-                print(Fore.LIGHTBLUE_EX + '\t\tDit resultat', resultat, Fore.RED + "er ikke et primtal")
+                print(Fore.LIGHTBLUE_EX + '\t\t\tDit resultat', resultat, Fore.RED + "er ikke et primtal")
                 break
             else:
-                print(Fore.LIGHTBLUE_EX + '\t\tDit resultat', resultat, Fore.GREEN + "er et primtal")
+                print(Fore.LIGHTBLUE_EX + '\t\t\tDit resultat', resultat, Fore.GREEN + "er et primtal")
+                break
     else:
-        print(Fore.LIGHTBLUE_EX + '\t\tDit resultat', resultat, Fore.RED + "er ikke et primtal")
+        print(Fore.LIGHTBLUE_EX + '\t\t\tDit resultat', resultat, Fore.RED + "er ikke et primtal")
+        break
+
+
+    print(wrapper.fill(Fore.BLUE + "\t  Hvis du vil lægge flere tal sammen så prøver vi en gang til"))
