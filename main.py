c3900262c01929d41867c2224011b64ce1afc099
Magnus H <mfh1106@gmail.com>
2023-02-05 01:48:14 +0100

    Update main.py
    Moving number calls into funktions to prevent calling them after the code has been executed once

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import textwrap
 # Importere colorama og tillader farvede outputs i console
 from colorama import Fore
+import math
 
 # Beder brugeren om deres navn for en mere personlig **Experience**
 navnet = input(Fore.BLUE + "NumZ: Skriv dit navn: ")
@@ -18,7 +19,6 @@ while True:
     prefix = Fore.GREEN + navnet + ": "
     prog = Fore.BLUE + "NumZ: "
 
-
     # Indsamler tal 1
     def talNummerEt():
         # Laver en wrapper til hvis der er meget store tal
@@ -30,6 +30,7 @@ while True:
             # int sørger for at string'en der er indtastet er et tal/integer
             global tal1
             tal1 = int(input(prefix))
+            print(Fore.LIGHTCYAN_EX + '\t\t\tDit første tal er:', tal1)
             talNummerTo()
 
         #       Hvis tallet ikke kan godkendes af maskinen eller
@@ -39,8 +40,6 @@ while True:
             print(wrapper.fill(Fore.RED + '\tIkke et Realt tal'))
             talNummerEt()
 
-        print(Fore.LIGHTCYAN_EX + '\t\t\tDit første tal er:', tal1)
-
 
     def talNummerTo():
         #   Indsamler tal 2
@@ -48,7 +47,8 @@ while True:
             print(wrapper.fill('Skriv dit andet tal'))
             global tal2
             tal2 = int(input(prefix))
-            math()
+            print(Fore.LIGHTCYAN_EX + '\t\t\tDit andet tal er', tal2)
+            mat()
 
         #       Hvis tallet ikke kan godkendes af maskinen eller
         #       brugeren har indtastet et bogstav giver den en besked + fejlkode
@@ -57,14 +57,11 @@ while True:
             print(wrapper.fill(Fore.RED + '\tIkke et Realt tal'))
             talNummerTo()
 
-        print(Fore.LIGHTCYAN_EX + '\t\t\tDit andet tal er', tal2)
 
-
-    def math():
+    def mat():
+        global resultat
         resultat = tal1 + tal2
 
-        #   Skaber en string der vil fortælle os om tallet er lige el. ulige
-        ligeEllerUlige = 'ikke bestemt'
         #   Tjekker om tallet er lige eller ulige
         if (resultat % 2) == 0:
             ligeEllerUlige = Fore.GREEN + 'lige'
@@ -74,22 +71,19 @@ while True:
         #   Lægger de 2 tal der er indsamlet i int strings sammen og viser om tallet er lige el. ulige på samme linje
         print(Fore.LIGHTBLUE_EX + '\t\t\tDin samlede værdi mellem de to tal er', resultat, 'resultatet er et',
               ligeEllerUlige, 'tal')
+        er_primtal()
 
         #   Finder ud af om resultatet er et primtal
-        #   Break efter et primtal resultat sørger for at tal som 4090 + 3 ikke bliver printet 4090 gange
-        #   Tak til @Kneecaptheif for at snakke om breakpoints og debug værktøjet
-        if resultat > 1:
-            for i in range(2, resultat // 2):
-                if (resultat % i) == 0:
-                    print(Fore.LIGHTBLUE_EX + '\t\t\tDit resultat', resultat, Fore.RED + "er ikke et primtal")
-                    break
-                else:
-                    print(Fore.LIGHTBLUE_EX + '\t\t\tDit resultat', resultat, Fore.GREEN + "er et primtal")
-                    break
-        else:
-            print(Fore.LIGHTBLUE_EX + '\t\t\tDit resultat', resultat, Fore.RED + "er ikke et primtal")
+
+
+    def er_primtal(resultat):
+        if resultat % 2 == 0 and resultat > 2:
+            return False
+        for i in range(3, int(math.sqrt(resultat)) + 1, 2):
+            if resultat % i == 0:
+                return False
+        return True
 
         print(wrapper.fill(Fore.BLUE + "\t  Hvis du vil lægge flere tal sammen så prøver vi en gang til"))
-
 
     talNummerEt()
